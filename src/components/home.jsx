2f08d259636287c275966aa946a20eb1a94dd9cc
nuraalhaji <danamonuraa@gmail.com>
2025-01-08 14:09:26 +0100

    added channel name

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,30 +1,33 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 const Home = () => {
-  const [bg, setBg] = useState('')
-  
-  const handBg = () => {
-    
-  
-    setBg(()=> num *num)
-  }
+  const [bg, setBg] = useState(true)
+  const [bg2, setBg2] = useState(true)
+  const [bg3, setBg3] = useState(true)
+  const [bg4, setBg4] = useState(true)
+  const [bg5, setBg5] = useState(true)
+
   
   
   return (
     <>
     <div>
-<img onMouseEnter={handBg} src='/dnatech.png'  width={100} height={40}/>
+<img  src='/dnatech.png'  width={100} height={40}/>
 </div>
 <div>
-<img  className='' src="DNATECH LOGO-1.jpg" width={300} height={350} alt="" />
+{bg5? (<div><img  onClick={()=>setBg5(false)} style={{borderRadius:"5px"}} src="DNATECH LOGO-1.jpg" width={300} height={350} alt="" /> </div>
+        ):(<div className='full2'><img  onClick={()=>setBg5(true)}  style={{position:"static"}}  src="DNATECH LOGO-1.jpg" width={500} height={600} alt="" /> </div>)}  
+   
+
 </div>
   <div >
     <h3   className='center' 
-    style={{fontStyle:"italic",color:' rgb(61, 31, 144)', fontWeight:'bolder'}}> SERVICES</h3>
+    style={{marginTop:"10px",fontStyle:"italic",color:' rgb(61, 31, 144)', fontWeight:'bolder'}}> SERVICES</h3>
   <div className="home">
 
-    <h4>Programming Front-End</h4>
-    <img  className='img' src="Capture2.PNG" width={150} height={100} alt="" />
+    <h4  style={{marginTop:"10px"}}>Programming Front-End</h4>
+      {bg? (<div><img  onClick={()=>setBg(false)} className='img' src="Capture2.PNG" width={150} height={100} alt="" /> </div>
+        ):(<div className='full'><img  onClick={()=>setBg(true)}  style={{position:"static"}} className='img' src="Capture2.PNG" width={1500} height={400} alt="" /> </div>)}  
     <ul>
       <li >HTML</li>
       <li >CSS</li>
@@ -32,7 +35,8 @@ const Home = () => {
     </ul>
 
       <h4>Advanced Front-End</h4>
-    <img  className='img' src="Capture.PNG" width={150} height={100} alt="" />
+      {bg2? (<div><img  onClick={()=>setBg2(false)} className='img' src="Capture.PNG" width={150} height={100} alt="" /> </div>
+        ):(<div className='full'><img  onClick={()=>setBg2(true)}  style={{position:"static"}} className='img' src="Capture.PNG" width={1500} height={400} alt="" /> </div>)}  
       
     <ul>
       <li >Reactjs</li>
@@ -40,8 +44,10 @@ const Home = () => {
       <li >NextJs</li>
       </ul>
       <h4>Programming Backend</h4> 
-    <img  className='img' src="Capture3.PNG" width={150} height={100} alt="" />
-
+    
+      {bg3? (<div><img  onClick={()=>setBg3(false)} className='img' src="Capture3.PNG" width={150} height={100} alt="" /> </div>
+        ):(<div className='full'><img  onClick={()=>setBg3(true)}  style={{position:"static"}} className='img' src="Capture3.PNG" width={1500} height={400} alt="" /> </div>)}  
+   
       <ul>
         <li >Python</li>
         <li >Rest Api</li>
@@ -50,6 +56,9 @@ const Home = () => {
       </ul>
 
     <h4>Graphic Design </h4>
+    {bg4? (<div><img  onClick={()=>setBg4(false)} className='img' src="Capture4.PNG" width={150} height={100} alt="" /> </div>
+        ):(<div className='full'><img  onClick={()=>setBg4(true)}  style={{position:"static"}} className='img' src="Capture4.PNG" width={1500} height={400} alt="" /> </div>)}  
+   
     <ul>
       <li >CorelDrew</li>
       <li >Photoshop</li>
